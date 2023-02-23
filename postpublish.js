c9c6088f558c519ccb23bb2bc536a807d7256b53
John Fanjoy <john.fanjoy@gmail.com>
2023-02-23 13:55:41 -0500

    update to use native promises and axios client

--- postpublish.js
+++ postpublish.js
@@ -1,24 +1,24 @@
 #! /usr/bin/env node
-'use strict';
+'use strict'
 
-var execSync = require('child_process').execSync;
-var version = require('./package.json').version;
+var { execSync } = require('child_process')
+var { version } = require('./package.json')
 
 if (!version) {
-    console.error('Can not determine current version');
-    process.exit(0);
+  console.error('Can not determine current version')
+  process.exit(0)
 }
 
-var tag = 'v' + version;
+var tag = 'v' + version
 try {
-    // if grep not found anything, it's exit code isn't zero, so execSync raises an Error
-    execSync('git tag | grep "' + tag + '"');
-    // it seems tag is found, do nothing
-    process.exit(0);
+  // if grep not found anything, it's exit code isn't zero, so execSync raises an Error
+  execSync('git tag | grep "' + tag + '"')
+  // it seems tag is found, do nothing
+  process.exit(0)
 } catch (e) {
-    // grep found nothing, so le'ts create new tag
-    console.info('creating a new tag: ', tag);
-    execSync('git tag ' + tag);
-    console.info('pushing tag to origin: ', tag);
-    execSync('git push origin ' + tag);
+  // grep found nothing, so le'ts create new tag
+  console.info('creating a new tag: ', tag)
+  execSync('git tag ' + tag)
+  console.info('pushing tag to origin: ', tag)
+  execSync('git push origin ' + tag)
 }

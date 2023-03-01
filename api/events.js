80741665a0f982ed002cf8a5610d134d5b760f43
John Fanjoy <john.fanjoy@gmail.com>
2023-03-01 09:45:18 -0500

    fix url search params for eventSource when filtering on event types

--- api/events.js
+++ api/events.js
@@ -4,24 +4,27 @@ var EventSource = require('eventsource')
 
 class MarathonEventSource {
   constructor (ctx) { // accepts a parent context
-    this.parent = ctx
+    this.basePath = ctx.basePath
     this.baseURL = ctx.baseURL
     this.baseURL.pathname = this.path
     this.client = ctx.http
     this.client.defaults.baseURL = this.baseURL.toString()
   }
 
-  get path () { return `${this.parent.basePath}/events` }
+  get path () { return `${this.basePath}/events` }
 
   // the constructor url should contain credentials and the api basepath '/v2'
   createEventSource (opts = {}) {
+    this.baseURL.pathname = this.path // make sure we have a good path
     const { eventType } = opts
     if (eventType) {
-      if (!((eventType instanceof String) || (eventType instanceof Array))) {
+      if (typeof eventType === 'string') {
+        this.baseURL.searchParams.set('event_type', eventType)
+      } else if (eventType instanceof Array) { // array
+        for (const type of eventType) { this.baseURL.searchParams.append('event_type', type) }
+      } else {
         throw new Error('"eventType" should be an array or string')
       }
-      this.baseURL.searchParams.set('event_type', eventType)
-      this.baseURL.pathname = this.path
     }
     this.es = new EventSource(`${this.baseURL}`)
     return this.es

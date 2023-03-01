220dc3afd419d731700f9aa284a45a95a83daa91
John Fanjoy <john.fanjoy@gmail.com>
2023-03-01 08:29:25 -0500

    make sure the baseURL gets set correctly for the eventSource

--- api/events.js
+++ api/events.js
@@ -6,10 +6,12 @@ class MarathonEventSource {
   constructor (ctx) { // accepts a parent context
     this.parent = ctx
     this.baseURL = ctx.baseURL
-    this.baseURL.pathname = `${ctx.basePath}/events`
+    this.baseURL.pathname = this.path
     this.client = ctx.http
     this.client.defaults.baseURL = this.baseURL.toString()
   }
+
+  get path () { return `${this.parent.basePath}/events` }
 
   // the constructor url should contain credentials and the api basepath '/v2'
   createEventSource (opts = {}) {
@@ -19,6 +21,7 @@ class MarathonEventSource {
         throw new Error('"eventType" should be an array or string')
       }
       this.baseURL.searchParams.set('event_type', eventType)
+      this.baseURL.pathname = this.path
     }
     this.es = new EventSource(`${this.baseURL}`)
     return this.es
